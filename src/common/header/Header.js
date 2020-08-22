941382037cf3b9371c364007f6af064b91f99b9f
sri-swetha <31824537+sri-swetha@users.noreply.github.com>
2020-08-22 16:59:27 +0530

    Added grid list for ‘Upcoming Movies

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,18 +3,59 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from'@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import PropTypes from 'prop-types'
+const TabContainer=function(props){
+    return(
+      <Typography component="div" style={{padding:0, textAlign:'center'}}>
+          {props.children}
+      </Typography>
+    );
+}
+TabContainer.propTypes={
+    children: PropTypes.node.isRequired
+}
 class Header extends Component{
     constructor(){
         super();
         this.state={
-            modalIsOpen: false
+            modalIsOpen: false,
+            value:0,
+            username:"",
+            userNameRequired:"dispNone",
+            password:"",
+            passwordRequired:"dispNone"
         }
     }
+    
     openHandler=()=>{
         this.setState({modalIsOpen:true})
     }
     closeHandler=()=>{
         this.setState({modalIsOpen:false})
+    }
+    tabChangeHandler=(event,value)=>{
+        this.setState({value})
+    }
+    loginClickHandler=()=>{
+        this.state.username === "" && this.state.password === "" ? this.setState({userNameRequired:"dispBlock"}) :
+         this.setState({userNameRequired:"dispNone"}) ;
+        this.state.username === "" ? this.setState({userNameRequired:"dispBlock"}) :
+         this.setState({userNameRequired:"dispNone"}) ;
+        this.state.password === "" ? this.setState({passwordRequired:"dispBlock" }) :
+         this.setState({passwordRequired: "dispNone"}) ;
+    }
+    inputUserNameChangeHandler=(e)=>{
+        this.setState({username: e.target.value});
+    }
+    inputPasswordChangeHandler=(e)=>{
+        this.setState({password: e.target.value});
     }
     render(){
         return(
@@ -23,7 +64,26 @@ class Header extends Component{
                     <img src={logo} className="app-logo" alt="logo" /> 
                     <Button id="loginBtn" variant="contained" onClick={this.openHandler}>Login</Button>           
                 </header>
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeHandler}></Modal>
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeHandler} id="modalId">
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler} className="tabs">
+                        <Tab label="Login"/>
+                        <Tab label="Register"/>
+                    </Tabs>
+                    {this.state.value === 0 &&
+                    <TabContainer>
+                        <FormControl required>
+                            <InputLabel htmlFor="userName">Username</InputLabel>
+                            <Input id="userName" type="text" username={this.state.username} onChange={this.inputUserNameChangeHandler} />
+                            <FormHelperText className={this.state.userNameRequired}><span className="red">required</span></FormHelperText>
+                        </FormControl><br/>
+                        <FormControl required>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="password" onChange={this.inputPasswordChangeHandler} />
+                            <FormHelperText className={this.state.passwordRequired}><span className="red">required</span></FormHelperText>
+                        </FormControl><br/><br/>
+                        <Button id="loginBtn" variant="contained" color='primary' onClick={this.loginClickHandler}>Login</Button> 
+                    </TabContainer>}
+                </Modal>
             </div>
         );
     }
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,13 +1,50 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './Home.css';
-import Header from'../../common/header/Header';
-class Home extends Component
-{
-    render(){
-        return(
-        <div>
-            <Header/>
-        </div>);
+import Header from '../../common/header/Header';
+import { withStyles } from '@material-ui/core/styles';
+import moviesData from '../../assets/movieData.js';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    upcomingMoviesHeading: {
+        textAlign: 'center',
+        background: '#ff9999',
+        padding: '8px',
+        fontSize: '1rem'
+    },
+    gridListUpcomingMovies: {
+        flexWrap: 'nowrap',
+        transform: 'translateZ(0)',
+        width: '100%'
+    }
+});
+
+class Home extends Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <Header />
+                <div className={classes.upcomingMoviesHeading}>
+                    <span>Upcoming Movies</span>
+                </div>
+                <GridList cols={5} className={classes.gridListUpcomingMovies}>
+                    {moviesData.map(movie=>(
+                        <GridListTile key= {movie.id}>
+                            <img src={movie.poster_url} alt={movie.title} />                        
+                            <GridListTileBar title={movie.title} />
+                        </GridListTile>
+                    ))}
+                    
+                </GridList>
+            </div>
+        )
     }
 }
-export default Home;
+
+export default withStyles(styles)(Home);
